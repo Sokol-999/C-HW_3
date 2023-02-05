b2389629396b7346f60765aaef50ddb9e508d022
Ilya Sokolov <sokolov.ilia.2000@mail.ru>
2023-02-06 02:44:03 +0300

    Homework done

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine("Write five digit number: ");
+string text = Console.ReadLine();
+if (text.Length == 5)
+{
+    if (text[0] == text[4] && text[1] == text[3])
+    {
+        System.Console.WriteLine("YES");
+    }
+    else
+    {
+        System.Console.WriteLine("NO");
+    }
+}
+else
+{
+    System.Console.WriteLine("It's not five digit number");
+}
+
+// Напишите программу, которая принимает на вход пятизначное 
+//число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты двух 
+//точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double[] coordsA = new double[3];
+double[] coordsB = new double[3];
+
+System.Console.WriteLine("Write coords of dot A: ");
+coordsA[0] = Convert.ToInt32(Console.ReadLine());
+coordsA[1] = Convert.ToInt32(Console.ReadLine());
+coordsA[2] = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Write coords of dot B: ");
+coordsB[0] = Convert.ToInt32(Console.ReadLine());
+coordsB[1] = Convert.ToInt32(Console.ReadLine());
+coordsB[2] = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(coordsA[0] - coordsB[0], 2) +
+Math.Pow(coordsA[1] - coordsB[1], 2) +
+Math.Pow(coordsA[2] - coordsB[2], 2));
+
+System.Console.WriteLine();
+System.Console.WriteLine($"RESULT: {result}");
