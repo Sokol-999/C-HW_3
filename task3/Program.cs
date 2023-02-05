444b08b9dfc4c532e0a1468f162219fe9006073d
Ilya Sokolov <sokolov.ilia.2000@mail.ru>
2023-02-06 02:52:49 +0300

    fixeg mistakes

--- task3/Program.cs
+++ task3/Program.cs
@@ -7,8 +7,15 @@
 System.Console.WriteLine("Wite N: ");
 
 int N = Convert.ToInt32(Console.ReadLine());
+if(N>0)
+{
 System.Console.WriteLine("Tablica korney: ");
 for (double i = 1; i <= N; i++)
 {
     System.Console.Write($"{Math.Pow(i, 3)} ");
+}
+}
+else
+{
+    System.Console.WriteLine("EROR: not an integer");
 }
